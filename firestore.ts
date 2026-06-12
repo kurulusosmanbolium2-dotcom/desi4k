f4989f0faf8c998332fe42d48dce519b9a7bbb80
kurulusosmanbolium2-dotcom <kurulusosmanbolium2@gmail.com>
2026-06-12 06:25:19 +0600

    Create firestore.ts

---
+++ firestore.ts
@@ -0,0 +1,15 @@
+import { collection, doc, getDocs, getDoc, updateDoc, increment, query, orderBy } from "firebase/firestore";
+import { db } from "./firebase";
+import type { Video } from "../types/video";
+
+export async function getAllVideos(): Promise<Video[]> {
+  const snap = await getDocs(query(collection(db, "video"), orderBy("views", "desc")));
+  return snap.docs.map((d) => ({ id: d.id, ...d.data() } as Video));
+}
+export async function getVideo(id: string): Promise<Video | null> {
+  const snap = await getDoc(doc(db, "video", id));
+  return snap.exists() ? { id: snap.id, ...snap.data() } as Video : null;
+}
+export async function incrementViews(id: string) {
+  await updateDoc(doc(db, "video", id), { views: increment(1) });
+                                                           }
